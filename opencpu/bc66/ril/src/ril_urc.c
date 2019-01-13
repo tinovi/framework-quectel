e4533e6549ccc675dd1c84c349faf51796f129cd
Georgi Angelov <the.wizarda@gmail.com>
2019-01-13 22:31:23 +0200

    Update ril_urc.c

--- opencpu/bc66/ril/src/ril_urc.c
+++ opencpu/bc66/ril/src/ril_urc.c
@@ -32,7 +32,7 @@
  ****************************************************************************/
 #include "custom_feature_def.h"
 #include "ql_stdlib.h"
-#include "ril.h "
+#include "ril.h"
 #include "ril_util.h"
 #include "ql_system.h"
 #include "ql_common.h"
