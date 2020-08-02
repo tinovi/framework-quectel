b1ceb9137b4110f29625691c6096dc1660002660
Georgi Angelov <the.wizarda@gmail.com>
2020-08-02 13:37:02 +0300

    Update hal.c

--- opencpu/bc66/SDK15/wizio/hal.c
+++ opencpu/bc66/SDK15/wizio/hal.c
@@ -15,6 +15,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+//
+// Helper for fast access
+// Usage: #include <hal.h>
+//
 ////////////////////////////////////////////////////////////////////////////
 
 #include <hal.h>
